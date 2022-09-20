36a4966dba2988c7c99a81d8268c522345a61eb5
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2022-09-20 20:42:20 +0400

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { YComponent } from './y/y.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServiceComponent } from './service/service.component';
+import { BlogComponent } from './blog/blog.component';
+import { SchoolComponent } from './school/school.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YComponent,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent,
+    HomeComponent,
+    ContactComponent,
+    PortfolioComponent,
+    ServiceComponent,
+    BlogComponent,
+    SchoolComponent
   ],
   imports: [
     BrowserModule
